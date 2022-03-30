13621bb7461e1daa232d59860d09877a0c6baca4
Abhishekgowda <abhishek@emproto.com>
2022-03-30 17:19:38 +0530

    change to add author-name, label, multiple statements, make changes in same file

--- src/main/resources/db/changelog/primary-v1.sql
+++ src/main/resources/db/changelog/primary-v1.sql
@@ -1,1 +1,5 @@
+--liquibase formatted sql
+--changeset abhishek:1 splitStatements:true endDelimiter:; labels:"pppp" runOnChange:true
 ALTER TABLE test_liquibase ADD "primary" varchar(50);
+
+--rollback alter table test_liquibase drop column "primary";
